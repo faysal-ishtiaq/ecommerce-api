383e7dd72b45467f0f7a891ecd15b175d9f010b2
Faysal Ishtiaq Rabby <f.i.rabby@gmail.com>
2018-03-27 12:27:25 +0600

    product and review seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,18 @@
 <?php
 
+use App\Model\Review;
+use App\Model\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
-     *
-     * @return void
      */
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        factory(Product::class, 146)->create();
+        factory(Review::class, 468)->create();
     }
 }
